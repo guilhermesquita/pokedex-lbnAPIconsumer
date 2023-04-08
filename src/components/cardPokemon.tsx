aa84f269b2e3c018ec8691875ff0cee5b9ddb05b
Guilherme  Mesquita <guilhermer_mesquita@hotmail.com>
2023-04-08 01:34:48 -0400

    feature: adjustment position components in cards

--- src/components/cardPokemon.tsx
+++ src/components/cardPokemon.tsx
@@ -1,23 +1,40 @@
-import { Card, Text, Flex } from "@chakra-ui/react"
+import { Card, Text, Flex, Button } from "@chakra-ui/react"
 import { Skills } from "./skills"
 
 export const CardPokemon = () => {
-    return(
-        <Flex width={'30%'} height={'25vh'} backgroundColor={'#729F92'} borderRadius={'12px'} paddingLeft={'30px'} 
-        paddingTop={'25px'} flexDirection={'column'}>
-            <Flex flexDirection={'column'}>
-                {/* NOME E NUMERAÇÃO DO POKEMON */}
-                <Flex>
-                    <Text fontSize={'15px'} fontFamily={'Inter'} fontWeight={700} color="#fff">#01</Text>
-                </Flex>
+	return (
+		<Flex width={'30%'} height={'27vh'} backgroundColor={'#729F92'} borderRadius={'12px'} paddingLeft={'30px'}
+			paddingTop={'25px'} flexDirection={'column'} paddingRight={'20px'}>
+			<Flex flexDirection={'column'}>
+				{/* NOME E NUMERAÇÃO DO POKEMON */}
+				<Flex>
+					<Text fontSize={'12px'} fontFamily={'Inter'} fontWeight={700} color="#fff">#01</Text>
+				</Flex>
 
-                <Flex>
-                    <Text fontSize={'70%'} fontFamily={'Inter'} fontWeight={700} color="#fff">Bulbasaur</Text>
-                </Flex>
-            </Flex>
+				<Flex>
+					<Text fontSize={'70%'} fontFamily={'Inter'} fontWeight={700} color="#fff">Bulbasaur</Text>
+				</Flex>
+			</Flex>
 
-            <Skills/>
+			<Flex flexDirection={'column'} gap={'30px'}>
 
-        </Flex>
-    )
+				<Flex gap={'2px'}>
+					<Skills />
+					<Skills />
+				</Flex>
+
+				<Flex justifyContent={'space-between'} borderColor={'red'} alignItems={'center'}>
+
+					<Text fontSize={'13px'} color={'#fff'} fontFamily={'Poppins'} fontWeight={700}
+						textDecoration={'underline'}>
+						<a href="#">Detalhes</a></Text>
+
+					<Button color={'black'} fontSize={'14px'} fontWeight={400}
+						width={'38%'} fontFamily={'Poppins'} height={'35px'}>Capturar!</Button>
+
+				</Flex>
+			</Flex>
+
+		</Flex>
+	)
 }

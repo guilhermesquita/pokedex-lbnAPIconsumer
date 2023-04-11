2286e6a7deec7a02951b02a79233354cd128a572
guilhermer_mesquita@hotmail.com <guilhermer_mesquita@hotmail.com>
2023-04-10 23:48:00 -0400

    feature: final style pokedex page

--- src/pages/PokedexPage.tsx
+++ src/pages/PokedexPage.tsx
@@ -1,10 +1,12 @@
-import { Card } from "@chakra-ui/react"
+import { Card, Flex } from "@chakra-ui/react"
 import { Header } from "../components/header"
 
 export const PokedexPage = () => {
   return(
     <Card>
 			<Header numberButton={1}/>
+			<Flex backgroundColor={'#5D5D5D'} h={'100vh'} w={'100%'} paddingTop={'60px'}
+      fontSize={'2.7rem'} paddingLeft={'90px'} flexDirection={'column'} gap={'50px'} paddingBottom={'50px'}></Flex>
 		</Card>
   )
 }
